04cbe756cc342786ad3c961c04dee332f8997ee8
seoyoung choi <seoyoungchoi@seoyoungui-MacBookPro.local>
2022-02-27 23:32:20 +0900

    Feat: [Post]signIn

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,30 @@
 const User = require("../models/User");
 
 const signIn = async (req, res, next) => {
+  const { email, nickname } = req.body;
+
   try {
-    console.log("something");
+    const user = await User.findOne({ email }).exec();
+
+    if (!user) {
+      await User.create({
+        email,
+        nickname,
+      });
+    }
+
+    res.json({
+      id: user.id,
+      email: user.email,
+      nickname: user.nickname,
+    });
   } catch (err) {
-    console.log(err);
+    res.json({
+      error: {
+        message: "Invalid Server Error",
+        code: 500,
+      },
+    });
   }
 };
 
